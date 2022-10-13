88d69ffded46883bca4085c4cd5d25577bd64d9c
Prajwal Bundhade <105680514+prajwalbundhade@users.noreply.github.com>
2022-10-13 11:42:51 +0530

    Update cd_practical5.c

--- cd_practical5.c
+++ cd_practical5.c
@@ -1,66 +1,58 @@
-#include<stdio.h>
-#include <stdlib.h>
-int main()
+#include <stdio.h>
+#include <string.h>
+int main() 
 {
-char exp[50];
-int symbol=0,number=0,id=0; char sym[20],ide[20];
-char num[20];
-int j=0,k=0,1=0,temp=0;
-printf("Enter the expression\n"); scanf("%s", &exp); //printf("%s",exp);
-int length=strlen(exp);
-for(inti=0;i<length;i++){
-if(exp[i] == ''||exp[i] == '+' ||exp[i] == '-' ||exp[i] =='*' || exp[i] == '/'||exp[i] ==','
-               ||exp[i] == ' ' ||exp[i] == '>' ||exp[i] =='<' ||exp[i] == '=' || exp[i] =='('
-               ||exp[i] == ')' ||exp[i] == '[' ||exp[i] ==']' ||exp[i] == '{' ||exp[i]== '}')
-     {      symbol++;
-            temp=exp[i]; 
-            exp[i]=sym[j]; 
-            sym[j]=temp;
-            j++;
-
-     }
-else if(exp[i] == '+' ||exp[i] == '' ||exp[i] == '*' || exp[i] =='/' ||exp[i] =='>' ||exp[i] =='<'
-             || exp[i] == "=")
-      {     symbol++;
-           temp=exp[i];
-           exp[i]=sym[j];
-           sym|[j]=temp;
-           j++;
-      }
-else if(exp[i] =='0' ||exp[i] == '1' ||exp[i] == '2' || exp[i] == '3' ||exp[i] == '4'||exp[i] == '5'
-            ||exp[i] == '6' ||exp[i] == '7' ||exp[i] == '8' ||exp[i] == '9' )
+    char expr[]="position = initial  + rate * 50 ; ";
+    char identifier[100],Digit[100],Symbols[100];
+    int j=0,k=0,n=0;
+    printf("Expression is :");
+    printf("%s\n",expr);
+    
+    for(int i=0;i<strlen(expr);i++)
     {
-            number++;
-            temp=exp[i];
-            exp[i]=num[k];
-            num[k]=temp;
-            k++;
-    }
-
-else{
-id++;
-temp=exp[i];
-exp[i]=ide[l];
-ide[l]=temp;
-l++;
-
-}
-
-printf("Identifiers=Y%d\n",id);
-for(int a=0;a<strlen(ide);a++){
-printf("%c\n"ide[a]);
-
-}
-
-printf("Symbol=%d\n",symbol);
-
-for(int b=0;b<strlen(sym);b++){
-printf("%c\n",sym[b]);
-
-printf("Number=%d\n",number);
-
-for(int i=0;i<strlen(num);i++){
-printf("%c\n",num[i]);
-
-}
+        char ch = expr[i];
+        if(ch=='a'||ch=='b'||ch=='c'||ch=='d'||ch=='e'||ch=='f'
+           ||ch=='g'||ch=='h'||ch=='i'||ch=='j'||ch=='k'||ch=='l'
+           ||ch=='m'||ch=='n'||ch=='o'||ch=='p'||ch=='q'||ch=='r'
+           ||ch=='s'||ch=='t'||ch=='u'||ch=='v'||ch=='w'||ch=='x'
+           ||ch=='y'||ch=='z')
+           {
+               identifier[j]=ch;
+               j++;
+           }
+          if(identifier[j-1]!=ch)
+          {
+              if(ch==' ')
+              {
+                  identifier[j]=' ';
+                  j++;
+              }
+          }
+          if(ch=='0'||ch=='1'||ch=='2'||ch=='3'||ch=='4'||ch=='5'
+             ||ch=='6'||ch=='7'||ch=='8'||ch=='9')
+             {
+                 Digit[k] = ch;
+                 k++;
+             }
+             
+            	
+             if(ch=='+'||ch=='-'||ch=='/'||ch=='*'||ch==';')
+             {
+                Symbols[n] = ch;
+                n++;
+             }
+          }
+          printf("\nIdentifiers are:");
+          for(int i=0;i<strlen(identifier);i++)
+          printf("%c",identifier[i]);
+          
+          printf("\nDigits are:");
+          for(int i=0;i<strlen(Digit);i++)
+          printf("%c",Digit[i]);
+          
+          printf("\nSymbols are:");
+          for(int i = 0; i<strlen(Symbols);i++)
+          printf("%c ",Symbols[i]);
+          return 0;
+    
 }
